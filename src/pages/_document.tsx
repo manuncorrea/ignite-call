58daf3f66c425c1c086d965b4a8b77f3d172be7b
Emanuele Correa <emanuele.correa@rhitmo.com.br>
2023-02-18 09:36:58 -0300

    installation design system

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '@ignite-ui/react'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Roboto:wght@500&display=swap" rel="stylesheet" />
+
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <body>
         <Main />
         <NextScript />
