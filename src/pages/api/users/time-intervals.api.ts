6a273a0f2fe79e68ebf7000f28cfdb0690e3f21c
Emanuele Correa <emanuele.correa@rhitmo.com.br>
2023-02-23 15:36:36 -0300

    bug: persisting in the range

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
+import { prisma } from '@/src/lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
+import { z } from 'zod'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+const timeIntervalBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,7 +28,23 @@ export default async function handler(
     buildNextAuthOptions(req, res),
   )
 
-  return res.json({
-    session,
-  })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          userId: session.user?.id,
+        },
+      })
+    }),
+  )
+  return res.status(201).end()
 }
