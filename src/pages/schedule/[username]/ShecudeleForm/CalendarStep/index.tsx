241320f4c49d3e8079a76256b6a813ac7daee86b
Emanuele Correa <emanuele.correa@rhitmo.com.br>
2023-03-04 14:38:13 -0300

    fix: loading available times

--- src/pages/schedule/[username]/ShecudeleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ShecudeleForm/CalendarStep/index.tsx
@@ -1,28 +1,50 @@
 import { Calendar } from '@/src/components/Calender'
+import { api } from '@/src/lib/axios'
 import dayjs from 'dayjs'
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import {
   Container,
   TimePicker,
   TimePickerHeader,
   TimePickerItem,
-  TimePickerList,
+  TimePickerList
 } from './styles'
 
 export function CalendarStep() {
-  const [selecteDate, setSelecteDate] = useState<Date | null>(null)
+  const [selectedDate, setSelecteDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
 
-  const isDateSelected = !!selecteDate
+  const router = useRouter()
 
-  const weekDay = selecteDate ? dayjs(selecteDate).format('dddd') : null
+  const isDateSelected = !!selectedDate
+  const username = String(router.query.username)
 
-  const describedDate = selecteDate
-    ? dayjs(selecteDate).format('DD[ de ]MMMM')
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
     : null
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((response) => {
+        console.log(response)
+      })
+  }, [selectedDate, username])
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar selectedDate={selecteDate} onDateSelected={setSelecteDate} />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelecteDate} />
 
       {isDateSelected && (
         <TimePicker>
